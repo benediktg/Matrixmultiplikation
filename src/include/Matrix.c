e8de2ec1a9a73396bbdaa6b3cdef4578ba85edb8
Benedikt Geißler <benedikt@g5r.eu>
2016-11-02 21:21:10 +0100

    Split up definition and declaration for Matrix structure

---
+++ src/include/Matrix.c
@@ -0,0 +1,51 @@
+#include "Matrix.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Matrix newMatrix(int rowCount, int columnCount)
+{
+    if ((rowCount < 0) || (columnCount < 0)) {
+        printf("Error: negative matrix size\n");
+        rowCount = 0;
+        columnCount = 0;
+    }
+    Matrix result;
+    result.rowCount = rowCount;
+    result.columnCount = columnCount;
+    result.data = malloc(rowCount * columnCount * sizeof(double));
+    return result;
+}
+
+bool isNullMatrix(Matrix m)
+{
+    if (m.rowCount == 0 && m.columnCount == 0 && m.data == NULL) {
+        return true;
+    }
+    return false;
+}
+
+double getElementValue(Matrix matrix, int i, int j)
+{
+    if ((i < 0) || (j < 0)) {
+        printf("Error: negative index\n");
+    } else if ((i >= matrix.rowCount) || (j >= matrix.columnCount)) {
+        printf("Error: too big index\n");
+    } else if (isNullMatrix(matrix)) {
+        printf("Error: null matrix\n");
+    }
+    return matrix.data[matrix.columnCount * i + j];
+}
+
+void setElementValue(Matrix *matrix, int i, int j, double value)
+{
+    if (matrix == NULL) {
+        printf("Error: null matrix\n");
+    } else if ((i < 0) || (j < 0)) {
+        printf("Error: negative index\n");
+    } else if ((i >= matrix->rowCount) || (j >= matrix->columnCount)) {
+        printf("Error: too big index\n");
+    }
+    matrix->data[matrix->columnCount * i + j] = value;
+}
--- src/include/Matrix.h
+++ src/include/Matrix.h
@@ -2,60 +2,31 @@
 #define MATRIX_H
 
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-typedef struct Matrix
-{
+typedef struct Matrix {
     int rowCount;
     int columnCount;
     double *data;
 } Matrix;
 
-Matrix createMatrix(int rowCount, int columnCount)
-{
-    if ((rowCount < 0) || (columnCount < 0)) {
-        printf("Error: negative matrix size\n");
-        rowCount = 0;
-        columnCount = 0;
-    }
-    Matrix result;
-    result.rowCount = rowCount;
-    result.columnCount = columnCount;
-    result.data = malloc(rowCount * columnCount * sizeof(double));
-    return result;
-}
+/*
+ * Constructs a Matrix instance and returns it.
+ */
+Matrix newMatrix(int rowCount, int columnCount);
 
-bool isNullMatrix(Matrix m)
-{
-    if (m.rowCount == 0 && m.columnCount == 0 && m.data == NULL) {
-        return true;
-    }
-    return false;
-}
+/*
+ * Checks if the matrix is similar to a null object.
+ */
+bool isNullMatrix(Matrix m);
 
-double getElementValue(Matrix matrix, int i, int j)
-{
-    if ((i < 0) || (j < 0)) {
-        printf("Error: negative index\n");
-    } else if ((i >= matrix.rowCount) || (j >= matrix.columnCount)) {
-        printf("Error: too big index\n");
-    } else if (isNullMatrix(matrix)) {
-        printf("Error: null matrix\n");
-    }
-    return matrix.data[matrix.columnCount * i + j];
-}
+/*
+ * Retrieves a value of a matrix element.
+ */
+double getElementValue(Matrix matrix, int i, int j);
 
-void setElementValue(Matrix *matrix, int i, int j, double value)
-{
-    if (matrix == NULL) {
-        printf("Error: null matrix\n");
-    } else if ((i < 0) || (j < 0)) {
-        printf("Error: negative index\n");
-    } else if ((i >= matrix->rowCount) || (j >= matrix->columnCount)) {
-        printf("Error: too big index\n");
-    }
-    matrix->data[matrix->columnCount * i + j] = value;
-}
+/*
+ * Sets a value of a matrix element.
+ */
+void setElementValue(Matrix *matrix, int i, int j, double value);
 
 #endif // MATRIX_H
