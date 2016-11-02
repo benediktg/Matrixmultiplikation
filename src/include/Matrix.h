549b956ccfe883aca782e621c49a8052d00d2d64
Benedikt Geißler <benedikt@g5r.eu>
2016-11-02 20:55:50 +0100

    Add simple error warnings

--- src/include/Matrix.h
+++ src/include/Matrix.h
@@ -1,6 +1,8 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct Matrix
@@ -12,6 +14,11 @@ typedef struct Matrix
 
 Matrix createMatrix(int rowCount, int columnCount)
 {
+    if ((rowCount < 0) || (columnCount < 0)) {
+        printf("Error: negative matrix size\n");
+        rowCount = 0;
+        columnCount = 0;
+    }
     Matrix result;
     result.rowCount = rowCount;
     result.columnCount = columnCount;
@@ -19,13 +26,35 @@ Matrix createMatrix(int rowCount, int columnCount)
     return result;
 }
 
+bool isNullMatrix(Matrix m)
+{
+    if (m.rowCount == 0 && m.columnCount == 0 && m.data == NULL) {
+        return true;
+    }
+    return false;
+}
+
 double getElementValue(Matrix matrix, int i, int j)
 {
+    if ((i < 0) || (j < 0)) {
+        printf("Error: negative index\n");
+    } else if ((i >= matrix.rowCount) || (j >= matrix.columnCount)) {
+        printf("Error: too big index\n");
+    } else if (isNullMatrix(matrix)) {
+        printf("Error: null matrix\n");
+    }
     return matrix.data[matrix.columnCount * i + j];
 }
 
 void setElementValue(Matrix *matrix, int i, int j, double value)
 {
+    if (matrix == NULL) {
+        printf("Error: null matrix\n");
+    } else if ((i < 0) || (j < 0)) {
+        printf("Error: negative index\n");
+    } else if ((i >= matrix->rowCount) || (j >= matrix->columnCount)) {
+        printf("Error: too big index\n");
+    }
     matrix->data[matrix->columnCount * i + j] = value;
 }
 
