1a7f6326354cd468da40429c61a58a3b97c8bb51
Benedikt Geißler <benedikt@g5r.eu>
2016-11-02 21:22:34 +0100

    Move main.c into src/

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "include/Matrix.h"
+#include "standard_mmul.c"
+#include "optimized_mmul.c"
+#include "parallel_mmul.c"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
