2ca78fd6f0da54e05b0e0eb5c24eda671dad6af8
Benedikt Geißler <benedikt@g5r.eu>
2016-11-02 22:09:50 +0100

    Add example execution in main.c

--- src/main.c
+++ src/main.c
@@ -8,5 +8,21 @@
 
 int main(int argc, char **argv)
 {
+    double d1[] = {
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+    };
+    Matrix m1 = newMatrix(3, 3);
+    m1.data = d1;
+    double d2[] = {
+        11, 12, 13,
+        14, 15, 16,
+        17, 18, 19,
+    };
+    Matrix m2 = newMatrix(3, 3);
+    m2.data = d2;
+
+    Matrix result = standardMatrixMul(m1, m2);
     return 0;
 }
