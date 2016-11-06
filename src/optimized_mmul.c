f8e3eca42276c4739318f9e8de98ca62954be773
Benedikt Geißler <benedikt@g5r.eu>
2016-11-06 15:12:02 +0100

    Print more specific error message in optimizedMatrixMul()

--- src/optimized_mmul.c
+++ src/optimized_mmul.c
@@ -4,10 +4,12 @@
 
 Matrix optimizedMatrixMul(Matrix a, Matrix b, int termination)
 {
-    if ((a.columnCount != b.rowCount)
-            || !(isSymmetricMatrix(a))
-            || !(isSymmetricMatrix(b))) {
+    if (a.columnCount != b.rowCount) {
         printf("Error: wrong matrix dimensions.\n");
+        return newMatrix(0, 0);
+    } else if (!(isSymmetricMatrix(a))
+            || !(isSymmetricMatrix(b))) {
+        printf("Error: please provide symmetric matrices.\n");
         return newMatrix(0, 0);
     }
 
