64036f7107e86f182e6508ff17b89907013a774c
Benedikt Geißler <benedikt@g5r.eu>
2016-12-03 15:34:30 +0100

    Fix time measurement

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -21,38 +22,61 @@ int main(int argc, char **argv)
     Matrix m1 = {MATRIX_SIZE, MATRIX_SIZE, data1};
     Matrix m2 = {MATRIX_SIZE, MATRIX_SIZE, data2};
 
+    float st, en;
     clock_t start, end;
-    float cpu_time_used;
+    float cpu_time_used, real_time_passed;
     Matrix result1 = allocMatrix(m1, m2);
     Matrix result2 = allocMatrix(m1, m2);
     Matrix result3 = allocMatrix(m1, m2);
 
     start = clock();
+    st = omp_get_wtime();
     standardMatrixMul(m1, m2, &result1);
     end = clock();
+    en = omp_get_wtime();
     cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
-    printf("time for standard:   %f seconds\n", cpu_time_used);
+    real_time_passed = (en - st);
+    printf("CPU  time for standard:   %f seconds\n"
+           "real time for standard:   %f seconds\n"
+           "\n",
+           cpu_time_used, real_time_passed);
 
     start = clock();
+    st = omp_get_wtime();
     optimizedMatrixMul(m1, m2, &result2, termination);
     end = clock();
+    en = omp_get_wtime();
     cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
-    printf("time for optimized:  %f seconds (termination: %d)\n",
-           cpu_time_used, termination
-    );
+    real_time_passed = (en - st);
+    printf("CPU  time for optimized:  %f seconds (termination: %d)\n"
+           "real time for optimized:  %f seconds (termination: %d)\n"
+           "\n",
+           cpu_time_used, termination, real_time_passed, termination);
 
     start = clock();
+    st = omp_get_wtime();
     parallelMatrixMul(m1, m2, &result3);
     end = clock();
+    en = omp_get_wtime();
     cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
-    printf("time for parallel:   %f seconds\n", cpu_time_used);
+    real_time_passed = (en - st);
+    printf("CPU  time for parallel:   %f seconds\n"
+           "real time for parallel:   %f seconds\n"
+           "\n",
+           cpu_time_used, real_time_passed);
 
     start = clock();
+    st = omp_get_wtime();
     bool equals = areEqualMatrices(result1, result2)
                   && areEqualMatrices(result2, result3);
     end = clock();
+    en = omp_get_wtime();
     cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
-    printf("time for comparison: %f seconds\n", cpu_time_used);
+    real_time_passed = (en - st);
+    printf("CPU  time for comparison: %f seconds\n"
+           "real time for comparison: %f seconds\n"
+           "\n",
+           cpu_time_used, real_time_passed);
 
     if (equals) {
         printf("correct result\n");
